7f3de458968e770573d08b5a84d9139eaa245d5d
SrKiwi2 <kcallisaya2.0@gmail.com>
2025-03-27 10:09:16 -0400

    Inicio

---
+++ src/main/java/com/usic/SistemasActivosFijosUAP/SistemasActivosFijosUapApplication.java
@@ -0,0 +1,13 @@
+package com.usic.SistemasActivosFijosUAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasActivosFijosUapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasActivosFijosUapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usic/SistemasActivosFijosUAP/SistemasActivosFijosUapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usic.SistemasActivosFijosUAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasActivosFijosUapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
