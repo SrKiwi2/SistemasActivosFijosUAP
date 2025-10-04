075fa4cb39142c98d34b889451fa2165731cdf48
Srkiwi2 <kcallisaya2.0@gmail.com>
2025-10-04 17:37:06 -0400

    servidfor_1

--- src/main/java/com/usic/SistemasActivosFijosUAP/config/PrimaryDataSourceConfig.java
+++ src/main/java/com/usic/SistemasActivosFijosUAP/config/PrimaryDataSourceConfig.java
@@ -2,19 +2,31 @@ package com.usic.SistemasActivosFijosUAP.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 @Configuration
 public class PrimaryDataSourceConfig {
-      @Bean
+@Bean
   @Primary
-  @ConfigurationProperties("spring.datasource")
-  public DataSource primaryDataSource() {
-    // Se construye con tus props: spring.datasource.url, username, password, driverClassName
-    return DataSourceBuilder.create().build();
+  public DataSource primaryDataSource(
+      @Value("${spring.datasource.url}") String url,
+      @Value("${spring.datasource.username}") String username,
+      @Value("${spring.datasource.password}") String password,
+      @Value("${spring.datasource.driverClassName}") String driver) {
+
+    HikariConfig cfg = new HikariConfig();
+    cfg.setJdbcUrl(url);              // <- forzamos jdbcUrl explícitamente
+    cfg.setUsername(username);
+    cfg.setPassword(password);
+    cfg.setDriverClassName(driver);
+    // (opcional) cfg.setMaximumPoolSize(10); etc.
+
+    return new HikariDataSource(cfg);
   }
 }
